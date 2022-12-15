dbf5bf4e69cfd8052efff9a8ee5aa5a183617fbc
Zaviel <zelvet0522@gmail.com>
2022-12-15 18:54:16 +0800

    adjust path name

---
+++ demo_1220/Helios_Dronecocntrol_Demo.py
@@ -0,0 +1,321 @@
+
+# stream processing
+import torch
+import numpy as np
+import cv2
+import time 
+import sys
+import av
+import random
+import openpifpaf
+
+
+# tello lib
+from djitellopy import Tello
+import tellopy
+
+# socket estimating and sending data 
+import socket
+import pickle
+import struct
+import imutils
+import threading
+import base64
+
+
+output = ""
+player=""
+fall = False
+hands_up = False
+
+class ObjectDetection:
+	def __init__(self):
+		self.predictor = self.load_model()
+		self.predictions = ""
+	def get_video_from_file(self):
+		self.drone = tellopy.Tello()
+		self.drone.connect()
+		self.drone.wait_for_connection(60.0)
+		retry = 3
+		self.container = None
+		while self.container is None and 0 < retry:
+			retry -= 1
+			try:
+				container = av.open(self.drone.get_video_stream())
+			except av.AVError as ave:
+				print(ave)
+				print('retry...')
+		assert container is not None
+		print("Tello initialized successfully")
+		return container.decode(video=0)
+
+	def load_model(self):
+		# mobilenetv3small, mobilenetv3large, resnet50, shufflenetv2k16, shufflenetv2k30
+		predictor = openpifpaf.Predictor(checkpoint='mobilenetv3large', json_data=True )
+		return predictor
+
+	def score_frame(self, frame):
+		predictions, gt_anns, image_meta = self.predictor.numpy_image(frame)
+		return predictions, gt_anns, image_meta
+
+	def plot_boxes(self, predictions, ok):
+		global fall
+		global hands_up
+		box_filter = ok.copy()
+		for l in range(len(predictions)):
+			# the color of each person's body dot
+			b, g, r = 255, 255, 255
+			# list title 
+			kp = predictions[l]["keypoints"] 
+			bbox = predictions[l]["bbox"]
+			# body line's width and color
+			body_trunk = 4
+			bgr_body = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
+			face_bgr = (0, 0, 255)		#	
+			neck_bgr = (255, 255, 255)	#
+			body_bgr = (100, 255, 77)	#
+
+			leftHand_bgr = (245, 0, 231)	#
+			leftLeg_bgr = (194, 102, 255)	#
+			rightHand_bgr = (149, 94, 255)	#
+			rightLeg_bgr = (237, 101, 23)	#
+
+			# --------------- Face ------------------	
+			if kp[0] and kp[3]: # nose to left eye
+				cv2.line(box_filter, (int(kp[0]),int(kp[1])), (int(kp[3]),int(kp[4])), face_bgr, body_trunk)	
+			if kp[0] and kp[6]: # nose to right eye
+				cv2.line(box_filter, (int(kp[0]),int(kp[1])), (int(kp[6]),int(kp[7])), face_bgr, body_trunk)	
+			if kp[6] and kp[12]:# right eye to ear
+				cv2.line(box_filter, (int(kp[6]),int(kp[7])), (int(kp[12]),int(kp[13])), face_bgr, body_trunk)	
+			if kp[3] and kp[9]: # left eye to ear
+				cv2.line(box_filter, (int(kp[3]),int(kp[4])), (int(kp[9]),int(kp[10])), face_bgr, body_trunk)	
+			if kp[0] and kp[15]: # nose to left shoulder
+				cv2.line(box_filter, (int(kp[0]),int(kp[1])), (int(kp[15]),int(kp[16])), neck_bgr, body_trunk)	
+			if kp[0] and kp[18]: # nose to right shoulder 
+				cv2.line(box_filter, (int(kp[0]),int(kp[1])), (int(kp[18]),int(kp[19])), neck_bgr, body_trunk)	
+			# --------------- Face ------------------
+
+			# --------------- Hands ------------------
+			if kp[24] and kp[18]: # right elbow to shoulder
+				cv2.line(box_filter, (int(kp[24]),int(kp[25])), (int(kp[18]),int(kp[19])), rightHand_bgr, body_trunk)	
+			if kp[21] and kp[15]: # left elbow to shoulder 
+				cv2.line(box_filter, (int(kp[21]),int(kp[22])), (int(kp[15]),int(kp[16])), leftHand_bgr, body_trunk)	
+			if kp[24] and kp[30]: # right elbow to wrist
+				cv2.line(box_filter, (int(kp[24]),int(kp[25])), (int(kp[30]),int(kp[31])), rightHand_bgr, body_trunk)	
+			if kp[21] and kp[27]: # left elbow to wrist
+				cv2.line(box_filter, (int(kp[21]),int(kp[22])), (int(kp[27]),int(kp[28])), leftHand_bgr, body_trunk)	
+			# ---------------- Hands ------------------
+
+			# ---------------- Body ------------------
+			if kp[15] and kp[18]: # left shoulder to right shoulder 
+				cv2.line(box_filter, (int(kp[15]),int(kp[16])), (int(kp[18]),int(kp[19])), body_bgr, body_trunk)
+			if kp[18] and kp[36]: # right shoulder to hip 
+				cv2.line(box_filter, (int(kp[36]),int(kp[37])), (int(kp[18]),int(kp[19])), body_bgr, body_trunk)	
+			if kp[15] and kp[33]: # left shoulder to hip
+				cv2.line(box_filter, (int(kp[15]),int(kp[16])), (int(kp[33]),int(kp[34])), body_bgr, body_trunk)	
+			if kp[33] and kp[36]: # right hip to left hip 
+				cv2.line(box_filter, (int(kp[33]),int(kp[34])), (int(kp[36]),int(kp[37])), body_bgr, body_trunk)	
+			# ---------------- Body ------------------
+
+			# ---------------- Legs ------------------
+			if kp[36] and kp[42]: # right hip to knee 
+				cv2.line(box_filter, (int(kp[36]),int(kp[37])), (int(kp[42]),int(kp[43])), rightLeg_bgr, body_trunk)	
+			if kp[33] and kp[39]: # left hip to knee
+				cv2.line(box_filter, (int(kp[33]),int(kp[34])), (int(kp[39]),int(kp[40])), leftLeg_bgr, body_trunk)	
+			if kp[42] and kp[48]: # right knee to right ankle  
+				cv2.line(box_filter, (int(kp[42]),int(kp[43])), (int(kp[48]),int(kp[49])), rightLeg_bgr, body_trunk)	
+			if kp[39] and kp[45]: # left knee to ankle  
+				cv2.line(box_filter, (int(kp[39]),int(kp[40])), (int(kp[45]),int(kp[46])), leftLeg_bgr, body_trunk)
+			# ---------------- Legs ------------------
+
+			# plot the body parts' dot 
+			i=0
+			while i<(len(kp)):
+				cv2.circle(box_filter,(int(kp[i]), int(kp[i+1])), 2, (b, g, r), -1)
+				i=i+3
+
+			# set upper and lower body's datum point to detect if this person fall  
+			face_datumpoint, body_datumpoint=-3,-3, 
+			if kp[1]>0:
+				face_datumpoint = int(kp[1])
+			elif kp[4]>0 and kp[7]>0:
+		        	face_datumpoint = (int(kp[4])+int(kp[7]))/2
+			elif kp[10]>0 and kp[13]>0:
+				face_datumpoint = (int(kp[10])+int(kp[13]))/2
+			elif kp[16]>0 and kp[19]>0:
+				face_datumpoint = (int(kp[16])+int(kp[19]))/2	
+
+
+			if kp[34]>0 and kp[37]>0:
+				body_datumpoint = ( int(kp[34]) + int(kp[37]) )/2 
+			elif kp[40]>0 and kp[43]>0:
+				body_datumpoint = ( int(kp[40]) + int(kp[43]) )/2 
+			elif kp[46]>0 and kp[49]>0:
+				body_datumpoint = ( int(kp[46]) + int(kp[49]) )/2 
+
+
+			# detect if someone gonna fall down
+			if abs( face_datumpoint - body_datumpoint ) < 30 and face_datumpoint!=-3 and body_datumpoint!=-3:
+				# plot on red frame if this person fall								
+				upl = int(bbox[0]), int(bbox[1])
+				buttomr = int(bbox[0]+bbox[2]), int(bbox[1]+bbox[3])
+				cv2.rectangle(box_filter, upl, buttomr, (0,0,255) , 1)		
+				cv2.putText(box_filter,"Fall detected", (int(bbox[0]),int(bbox[1])-5), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,255), 1)	
+				fall = True
+
+
+			# set hands up pose detection
+			hands  = -3
+			if kp[22]>0 and kp[25]>0:
+				hands = (int(kp[22])+int(kp[25]))/2
+			if kp[28]>0 and kp[31]>0:
+				hands = (int(kp[28])+int(kp[31]))/2
+
+			if ( face_datumpoint - hands)  > 30 and hands!=-3:
+				# plot on the frame if this person put her hands up
+				upl = int(bbox[0])+2, int(bbox[1])+2
+				buttomr = int(bbox[0]+bbox[2]), int(bbox[1]+bbox[3])
+				cv2.rectangle(box_filter, upl, buttomr, (0,255,255) , 1)
+				cv2.putText(box_filter,"Hands up", (int(bbox[0]),int(bbox[1])-5), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,255,255), 1)
+				hands_up  = True	
+	
+
+		Transparency = 0.7
+		ok = cv2.addWeighted(box_filter, Transparency, ok, 1 - Transparency, 0)
+
+		return ok, fall
+
+	def modeling(self):
+		while True:
+			self.predictions, gt_anns, image_meta = self.score_frame(self.frame)
+
+			"""
+			st = time.time()
+			et = time.time()
+			elapsed_time = et - st
+			print('Execution time:', elapsed_time, 'seconds')
+			"""
+	def __call__(self):
+		global player
+		global output
+
+		player = self.get_video_from_file() # create streaming service for application
+
+		frame_skip = 300
+		for frame in player:	
+			if 0 < frame_skip:
+				frame_skip = frame_skip - 1
+				continue
+			break
+
+		WIDTH =400
+		for frame in player:
+			frame = cv2.cvtColor(np.array(frame.to_image()), cv2.COLOR_RGB2BGR)
+			self.frame = imutils.resize(frame,width=WIDTH)
+			output=self.frame
+
+			stream_modeling = threading.Thread(target = self.modeling)
+			#Server_processing = threading.Thread(target = Server_process)
+			input_getting = threading.Thread(target = get_input)
+			stream_modeling.daemon = True	
+			#Server_processing.daemon = True	
+			input_getting.daemon = True
+			stream_modeling.start()
+			#Server_processing.start()
+			input_getting.start()
+			time.sleep(2)
+			break
+
+		print("-------- Thread initialized --------")
+
+
+		try:
+			for frame in player:
+				frame = cv2.cvtColor(np.array(frame.to_image()), cv2.COLOR_RGB2BGR)		
+				self.frame = imutils.resize(frame,width=WIDTH)
+				output, fall = self.plot_boxes(self.predictions, self.frame)
+				"""
+				if fall:
+					print("Warning! Warning!")
+				"""
+				local_show = imutils.resize(output, 700)
+				cv2.imshow("ewe",local_show)
+				key = cv2.waitKey(1)
+				if key == ord('g'):
+					self.drone.land()
+				if key == ord('t'):
+					self.drone.takeoff()
+				if key == ord('r'):
+					self.drone.set_throttle(0.5)
+				if key == ord('f'):
+					self.drone.set_throttle(-0.5)
+				if key == ord('e'):
+					self.drone.set_yaw(0.5)
+				if key == ord('q'):
+					self.drone.set_yaw(-0.5)
+				if key == ord('z'):
+					self.drone.set_throttle(0)
+					self.drone.set_yaw(0)
+		except SystemExit:
+			self.drone.land()
+
+sent = b""
+def get_input():
+	global sent
+	while True:
+		data = b""
+		while not data:
+			data = input().encode()
+		sent = data
+
+def Server_process():
+	global fall
+	global hands_up
+	global sent
+	# Server socket
+	# create an INET, STREAMing socket
+	server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	host_name  = socket.gethostname()
+	host_ip = socket.gethostbyname(host_name)
+
+	print('HOST IP:', local_ip)
+	port = 60050
+	socket_address = (local_ip, port)
+
+
+	# bind the socket to the host. 
+	#The values passed to bind() depend on the address family of the socket
+	server_socket.bind(socket_address)
+	print('Socket bindng completed')
+	#listen() enables a server to accept() connections
+	#listen() has a backlog parameter. 
+	#It specifies the number of unaccepted connections that the system will allow before refusing new connections.
+
+	indata, Client_addr = server_socket.recvfrom(1024)
+	while True:
+		print('Connection from:',Client_addr)
+		WIDTH=500
+		while(True):
+			if fall:
+				server_socket.sendto("fall".encode(), Client_addr)
+				fall = False
+			if hands_up:
+				server_socket.sendto("hands_up".encode(), Client_addr)
+				hands_up = False
+			if sent:
+				server_socket.sendto(sent, Client_addr)	
+				sent = b""
+			try:
+				encoded,buffer = cv2.imencode('.jpg',output)
+				message = base64.b64encode(buffer)
+				server_socket.sendto(message,Client_addr)
+			except:
+				pass
+
+if __name__ == "__main__":
+	a = ObjectDetection()
+	a()
+
+
+
---
+++ helios_app/main.py
@@ -0,0 +1,202 @@
+
+# file and window manager import
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.config import Config
+# widget import
+from kivy_garden.mapview import MapView
+from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+from kivy.uix.button import Button
+# screen stuff
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition, NoTransition
+from kivy.properties import StringProperty, ObjectProperty
+from kivy.lang import Builder
+# socket and stream processing stuff import
+from kivy.clock import Clock
+from kivy.clock import CyClockBaseFree
+import socket
+import cv2
+import pickle
+import struct
+import imutils
+import numpy as np
+import base64
+import sys
+import time
+
+
+stream_on = False
+fall = False
+hands_up = False
+N = 0
+
+# handling local ip and server ip
+try:
+	local_ip = sys.argv[1]
+	server_ip = sys.argv[2]
+	if local_ip == "--help":
+		print("usage: python3 webcam_openpifpaf.py [self_IP_addr] [server_IP_addr]")
+		exit()
+except:
+	print("error : missing local IP parameter")
+	print("usage: python3 webcam_openpifpaf.py [self_IP_addr] [server_IP_addr]")
+	exit()
+
+# app establishing import
+from kivy.app import App
+
+class UI_filter(Widget):
+	pass
+
+class Profile_page(Screen):
+	pass
+
+class Drone_page(Screen):
+	pass
+#	def on_pre_enter(self, *args):
+
+
+class Stream_page(Screen):
+	pass 
+class Stream(Image):
+	def __init__(self,  **kwargs):
+		super(Stream, self).__init__(**kwargs)
+		self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.port = 60050 
+		socket_address = (local_ip, self.port)
+		#socket_address = ('10.22.48.120',self.port)
+		self.client_socket.bind(socket_address)
+		print("Client binded")
+		data = b""
+		payload_size = struct.calcsize("Q")
+		ip=(server_ip,self.port)
+		#ip=('10.22.75.87',self.port)
+		self.client_socket.sendto(b"ewe!",ip)
+		Clock.schedule_interval_free(self.update, 0.017)
+	
+	def update(self, dt):
+		global fall
+		global hands_up
+		global N 
+		packet,_ = self.client_socket.recvfrom(65536)
+		if packet.decode() == "fall":
+			fall = True
+		elif packet.decode() == "hands_up":
+			hands_up = True
+		else:	
+			try:
+				start_time = time.time()
+				data = base64.b64decode(packet,' /')
+				npdata = np.frombuffer(data,dtype=np.uint8)
+				frame = cv2.imdecode(npdata,1)
+				frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+				frame = imutils.resize(frame, 1200)
+				buf1 = cv2.flip(frame, 0)
+				buf = buf1.tobytes()
+				image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+				image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+				# display image from the texture
+				self.texture = image_texture
+				#print("FPS: ", 1.0 / (time.time() - start_time)) # FPS = 1 / time to process loop
+			except:
+				if N == 0:
+					App.get_running_app().root.get_screen('list').ids.notification_2.text = packet.decode()
+					N = N + 1
+				elif N == 1:
+					App.get_running_app().root.get_screen('list').ids.notification_3.text = packet.decode()
+					N = N + 1
+				elif N == 2:
+					App.get_running_app().root.get_screen('list').ids.notification_4.text = packet.decode()
+					N = N + 1
+				elif N == 3:
+					App.get_running_app().root.get_screen('list').ids.notification_5.text = packet.decode()
+					N = N + 1
+				elif N == 4:
+					App.get_running_app().root.get_screen('list').ids.notification_6.text = packet.decode()
+					N = N + 1
+					
+
+class RunnerList_page(Screen):
+	pass
+
+class Notifications_page(Screen):
+	pass
+
+class Map_page(Screen):
+	pass
+
+
+class ScreenManager(ScreenManager):
+	pass
+
+
+
+
+class main(App):
+	def build(self):
+		Config.set('kivy', 'kivy_clock', 'free_all')
+		Window.size = (321, 694.5)
+		Clock.schedule_interval_free(self.notice,3)
+	def notice(self, dt):
+		global fall
+		global hands_up
+
+		if fall:
+			App.get_running_app().root.get_screen('list').ids.notification_1.text = "Drn_3 fall warning !"
+			App.get_running_app().root.get_screen('list').ids.notification_1.color = 255,0,0,1
+			App.get_running_app().root.get_screen('drone').ids.drn3_notice.source = "drawable/red_notice.png"
+			App.get_running_app().root.get_screen('drone').ids.drn3_notice.size_hint_x = .12		
+			
+			App.get_running_app().root.get_screen('drone').ids.notice_drone.source = "drawable/red_notice.png"
+			App.get_running_app().root.get_screen('drone').ids.notice_drone.size_hint_x = .1
+			App.get_running_app().root.get_screen('list').ids.notice_drone.source = "drawable/red_notice.png"
+			App.get_running_app().root.get_screen('list').ids.notice_drone.size_hint_x = .1
+			App.get_running_app().root.get_screen('profile').ids.notice_drone.source = "drawable/red_notice.png"
+			App.get_running_app().root.get_screen('profile').ids.notice_drone.size_hint_x = .1
+			App.get_running_app().root.get_screen('map').ids.notice_drone.source = "drawable/red_notice.png"
+			App.get_running_app().root.get_screen('map').ids.notice_drone.size_hint_x = .1
+			App.get_running_app().root.get_screen('notification').ids.notice_drone.source = "drawable/red_notice.png"
+			App.get_running_app().root.get_screen('notification').ids.notice_drone.size_hint_x = .1
+			fall = False
+		else:
+			App.get_running_app().root.get_screen('drone').ids.drn3_notice.size_hint_x = 0		
+			
+			App.get_running_app().root.get_screen('drone').ids.notice_drone.size_hint_x = 0
+			App.get_running_app().root.get_screen('list').ids.notice_drone.size_hint_x = 0
+			App.get_running_app().root.get_screen('profile').ids.notice_drone.size_hint_x = 0
+			App.get_running_app().root.get_screen('map').ids.notice_drone.size_hint_x = 0
+			App.get_running_app().root.get_screen('notification').ids.notice_drone.size_hint_x = 0
+		if hands_up:
+			App.get_running_app().root.get_screen('list').ids.notification_1.text = "Drn_3 hamds up warning !"
+			App.get_running_app().root.get_screen('list').ids.notification_1.color = 255,0,0,1
+			App.get_running_app().root.get_screen('drone').ids.drn3_notice.source = "drawable/purple_notice.png"
+			App.get_running_app().root.get_screen('drone').ids.drn3_notice.size_hint_x = .12	
+			
+			App.get_running_app().root.get_screen('drone').ids.notice_drone.source = "drawable/purple_notice.png"
+			App.get_running_app().root.get_screen('drone').ids.notice_drone.size_hint_x = .1	
+			App.get_running_app().root.get_screen('list').ids.notice_drone.source = "drawable/purple_notice.png"
+			App.get_running_app().root.get_screen('list').ids.notice_drone.size_hint_x = .1	
+			App.get_running_app().root.get_screen('profile').ids.notice_drone.source = "drawable/purple_notice.png"
+			App.get_running_app().root.get_screen('profile').ids.notice_drone.size_hint_x = .1	
+			App.get_running_app().root.get_screen('map').ids.notice_drone.source = "drawable/purple_notice.png"
+			App.get_running_app().root.get_screen('map').ids.notice_drone.size_hint_x = .1	
+			App.get_running_app().root.get_screen('notification').ids.notice_drone.source = "drawable/purple_notice.png"
+			App.get_running_app().root.get_screen('notification').ids.notice_drone.size_hint_x = .1	
+			hands_up = False
+		else:
+			App.get_running_app().root.get_screen('drone').ids.drn3_notice.size_hint_x = 0	
+			
+			App.get_running_app().root.get_screen('drone').ids.notice_drone.size_hint_x = 0	
+			App.get_running_app().root.get_screen('list').ids.notice_drone.size_hint_x = 0	
+			App.get_running_app().root.get_screen('profile').ids.notice_drone.size_hint_x = 0
+			App.get_running_app().root.get_screen('map').ids.notice_drone.size_hint_x = 0
+			App.get_running_app().root.get_screen('notification').ids.notice_drone.size_hint_x = 0
+
+
+if __name__ == "__main__":
+	main().run()
+
+
+
